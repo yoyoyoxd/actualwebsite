fac2bf983d4a520df57b692567c9c1f260e71c34
crypto-b <>
2021-02-06 16:38:04 +0100

    stats styling

---
+++ content/en/homepage_faqs/no_rewards.md
@@ -0,0 +1,9 @@
+---
+title: "I'm not seeing reward transactions in my wallet?"
+date: 2021-02-03T21:23:03+01:00
+draft: false
+---
+
+The rewards mechanism built into the contract aren't distributed via individual blockchain transactions, 
+but if you look at your balance on Etherscan or your preferred Eth explorer, you can see the rewards in 
+your wallet token balance.
--- docs/js/main.js
+++ docs/js/main.js
@@ -1,6 +1,12 @@
-import { CountUp } from "https://cdnjs.cloudflare.com/ajax/libs/countup.js/2.0.7/countUp.min.js";
+import {CountUp} from "https://cdnjs.cloudflare.com/ajax/libs/countup.js/2.0.7/countUp.min.js";
 import TransactionStats from "./TransactionStats.js";
+import Calculator, {Events as CalcEvents} from "./Calculator.js";
 import CONTRACT_ABI from "./abi.js";
+
+function getElByID(id) {
+    return document.getElementById(id);
+};
+
 const stats = new TransactionStats('0x13572851103bed49ff743af4c4bb5ace88b22e2f', CONTRACT_ABI, 'wss://mainnet.infura.io/ws/v3/4dd869b552454cc79348980651f943dd');
 // roughly a day's worth of blocks
 stats.getTransactionStats(6498).then((stats) => {
@@ -11,8 +17,27 @@ stats.getTransactionStats(6498).then((stats) => {
     feesCnt.start();
 });
 
-$(function () {
+if (getElByID('calculator')) {
+    const calc = new Calculator(
+        5000000,
+        5,
+        [50000, 100000, 150000, 200000, 250000, 500000, 750000, 1000000, 2000000, 3000000, 5000000, 10000000],
+        getElByID('calc_holdings'),
+        getElByID('calc_volume')
+    );
+    calc.on(CalcEvents.CALC_EVENT_CALCULATE, (rewards) => {
+        const rewardsCnt = new CountUp('calc_results', rewards);
+        rewardsCnt.start();
+    });
 
+    calc.on(CalcEvents.CALC_EVENT_VOLUME, (volume) => {
+        getElByID('calc_volume_val').innerHTML = volume.toLocaleString();
+    });
+
+    calc.calculate();
+}
+
+$(function () {
     function setMenuBg() {
         const nav = $('.main-nav');
         if ($(window).scrollTop() === 0) {
